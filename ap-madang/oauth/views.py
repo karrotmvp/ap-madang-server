31ac59dffd3e5e7cbf6f780dcbc71e09b5a8744e
ImagineHJ <hj0816hj@gmail.com>
2021-10-05 19:02:15 +0900

    feat: get user info from oauth

--- ap-madang/oauth/views.py
+++ ap-madang/oauth/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 import requests
+from config.settings import APP_ID_SECRET
+
+BASE_URL = "https://openapi.kr.karrotmarket.com"
 
 # Create your views here.
 
-def me(request):
-    # code 받아오기
-    code = request.GET.get("code", None)
-    
-    # auth token 받아오기
-    
-    url = "https://openapi.kr.karrotmarket.com/oauth/token"
+
+def get_access_token_from_code(code):
+    url = BASE_URL + "/oauth/token"
 
     querystring = {
         "code" : code,
@@ -17,10 +16,25 @@ def me(request):
         "scope" : "account/profile",
         "response_type" : "code"
     }
-    headers = {"Authorization": "Basic " + "appId:appSecret" }
+
+    headers = {"Authorization": "Basic " + APP_ID_SECRET }
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     print(response.text)
+    return response.text.get("access_token", None)
+
+def get_user_info(access_token):
+    url = BASE_URL + "/api/v1/users/me"
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Bearer " + access_token
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    return response.text
 
 
-    # token 으로 유저 정보 받아오기
+
+
+    
