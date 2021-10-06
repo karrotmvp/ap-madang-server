eaa3df83afb0311f915af29761bbdc7f859eea09
ImagineHJ <hj0816hj@gmail.com>
2021-10-06 16:57:39 +0900

    feat: add config

--- ap-madang/config/settings.py
+++ ap-madang/config/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
