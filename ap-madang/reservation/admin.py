c7dfe711d597f47cfefdd709552e6ab7cd170e21
ImagineHJ <hj0816hj@gmail.com>
2021-10-07 14:40:00 +0900

    feat: add register admin

--- ap-madang/reservation/admin.py
+++ ap-madang/reservation/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Reservation
 
 # Register your models here.
-admin.site.register(Reservation)
+# admin.site.register(Reservation)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'region', 'suggestion', 'created_at')
