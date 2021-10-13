85b8e3ecc659e4fd709723eac43547be38f52f22
ImagineHJ <hj0816hj@gmail.com>
2021-10-13 12:26:03 +0900

    feat: add User model

--- ap-madang/user/models.py
+++ ap-madang/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class User(Base):
+    karrot_user_id = models.CharField( max_length=200, unique=True)
+    nickname = models.CharField( max_length=200 )
+    profile_image_url = models.TextField( blank=True, null=True )
+    temperature = models.IntegerField()
+
+    def __str__(self):
+        return self.nickname
