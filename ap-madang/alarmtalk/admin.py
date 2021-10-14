a44bbf3e6f0bc89a3ce4666e77a3ed9a6fbb3546
ImagineHJ <hj0816hj@gmail.com>
2021-10-14 18:56:02 +0900

    feat: create alarm talk app

---
+++ ap-madang/alarmtalk/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import UserMeetingAlarm
+
+# Register your models here.
+@admin.register(UserMeetingAlarm)
+class UserMeetingAlarmAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "meeting", "created_at")
---
+++ ap-madang/alarmtalk/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlarmtalkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alarmtalk'
