8c2c55f996039dc4da353598acac3f234d640293
ImagineHJ <hj0816hj@gmail.com>
2021-10-14 17:04:10 +0900

    chore: delete unused serializer

--- ap-madang/reservation/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class ReservationBodySerializer(serializers.Serializer):
-    code = serializers.CharField(help_text="Oauth 인가 코드")
-    region_id = serializers.CharField(help_text="지역 아이디")
-    suggestion = serializers.CharField(allow_blank=True, allow_null=True, help_text="유저 주관식 답안")
