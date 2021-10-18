88059ff5e3c97dc55172ccde71513abbba0630b1
ImagineHJ <hj0816hj@gmail.com>
2021-10-18 17:11:23 +0900

    feat: add user opinion model

--- ap-madang/support/models.py
+++ ap-madang/support/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserOpinion(Base):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
