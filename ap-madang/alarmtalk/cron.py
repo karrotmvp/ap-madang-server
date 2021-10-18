62d84e787dd0ba133024ed68a1875ca57fcebdd8
ImagineHJ <hj0816hj@gmail.com>
2021-10-18 12:26:07 +0900

    feat: add exception handling, time compare

--- ap-madang/alarmtalk/cron.py
+++ ap-madang/alarmtalk/cron.py
@@ -4,30 +4,33 @@ from .models import *
 from .views import *
 
 
+def cron_test():
+    print("cron job is working!", str(datetime.now()))
+
+
 def send_meeting_alarm():
-    title = "모임이 시작됐어요"
-    text = ""
-    primary_button_url = ""
+    title = " 모임이 시작됐어요"
+    text = '아래 "모임 바로가기" 버튼을 눌러 이웃과 대화를 나눠보세요.'
     primary_button_text = "모임 바로가기"
     total_alarm_num = 0
 
     # 현재 시간에 열리는 모임 리스트 가져오기
-    now = datetime.now().time()
-    meetings = Meeting.objects.filter(start_time=now)
+    now = datetime.strftime(datetime.now(), "%H:%M:%000")
+    meetings = Meeting.objects.filter(is_deleted=False, start_time=now)
 
     # 해당 모임 예약 내역 가져오기
     alarm_list = UserMeetingAlarm.objects.filter(sent_at=None, meeting__in=meetings)
-
-    print("-----user meeting alarm start " + now + " -----")
+    print("----- user meeting alarm start : " + str(now) + " -----")
 
     for alarm in alarm_list:
         if send_biz_chat_message(
             alarm.user.karrot_user_id,
-            title,
+            alarm.meeting.title + title,
             text,
-            primary_button_url,
+            alarm.meeting.meeting_url,
             primary_button_text,
         ):
+            print("Alarm sent! to ", alarm.user.karrot_user_id)
             alarm.sent_at = datetime.now()
             alarm.save()
             total_alarm_num += 1
@@ -36,4 +39,8 @@ def send_meeting_alarm():
             # TODO 우리한테 노티스 보내기? 아니면 retry 하기?
             pass
 
-    print("-----user meeting alarm end with " + now + " alarm talks total -----")
+    print(
+        "----- user meeting alarm end with : " + str(now) + " alarm talks total ",
+        total_alarm_num,
+        "-----",
+    )
