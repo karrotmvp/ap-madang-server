db6b63cf01b2cb6b619baa5e1f4833a2bca1d10a
ImagineHJ <hj0816hj@gmail.com>
2021-10-18 17:11:44 +0900

    feat: register user opinion in admin

--- ap-madang/support/admin.py
+++ ap-madang/support/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(UserOpinion)
+class UserOpinionAdmin(admin.ModelAdmin):
+    list_display = ("user", "body", "created_at")
