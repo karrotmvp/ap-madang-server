0c1961828a68df799e2777eb093f6aab59511a62
ImagineHJ <hj0816hj@gmail.com>
2021-10-21 15:54:16 +0900

    feat: create meeting log 2 days early

--- ap-madang/meeting/cron.py
+++ ap-madang/meeting/cron.py
@@ -7,20 +7,18 @@ def create_tomorrows_meeting():
     print("----- meeting log creation start : " + str(datetime.now()) + " -----")
     total = 0
 
-    tomorrow = date.today() + timedelta(days=1)
-    meetings = Meeting.objects.filter(
-        is_deleted=False, days__day=get_date_of_tomorrow()
-    )
+    day_aft_tom = date.today() + timedelta(days=2)
+    meetings = Meeting.objects.filter(is_deleted=False, days__day=get_date(day_aft_tom))
     meetings_num = meetings.count()
 
     for meeting in meetings:
-        MeetingLog.objects.create(meeting=meeting, date=tomorrow)
+        MeetingLog.objects.create(meeting=meeting, date=day_aft_tom)
         total += 1
 
     if total != meetings_num:
-        capture_message(str(tomorrow) + "의 모임 로그가 생성되지 않았습니다.")
+        capture_message(str(day_aft_tom) + "의 모임 로그가 생성되지 않았습니다.")
     else:
         print("created " + str(total) + " meeting logs")
 
-    print("----- meeting log creation start : " + str(datetime.now()) + " -----")
+    print("----- meeting log creation end : " + str(datetime.now()) + " -----")
     print()
