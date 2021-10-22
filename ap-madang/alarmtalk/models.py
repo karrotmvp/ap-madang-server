ec6dcf421118fe18a8e8b2f6ac09e2d3c837f43a
ImagineHJ <hj0816hj@gmail.com>
2021-10-22 12:32:23 +0900

    feat: add user meeting alarm constraint

--- ap-madang/alarmtalk/models.py
+++ ap-madang/alarmtalk/models.py
@@ -16,5 +16,13 @@ class UserMeetingAlarm(Base):
     meeting = models.ForeignKey(MeetingLog, on_delete=models.CASCADE)
     sent_at = models.DateTimeField(blank=True, null=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "meeting"],
+                name="only one alarm for each meeting - user",
+            ),
+        ]
+
     def __str__(self):
         return self.user.nickname + " - " + self.meeting.meeting.title
