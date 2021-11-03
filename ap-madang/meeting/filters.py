d9215523df9f594cd4532a931c56d7914a95b944
ImagineHJ <hj0816hj@gmail.com>
2021-11-03 17:18:03 +0900

    fix: fix date filter

--- ap-madang/meeting/filters.py
+++ ap-madang/meeting/filters.py
@@ -10,6 +10,7 @@ class CustomDateFieldListFilter(DateFieldListFilter):
         yesterday = today - timedelta(days=1)
         tomorrow = today + timedelta(days=1)
         day_after_tomorrow = today + timedelta(days=2)
+        two_days_after_tomorrow = today + timedelta(days=3)
 
         self.links = list(self.links)
         self.links.insert(
@@ -27,8 +28,8 @@ class CustomDateFieldListFilter(DateFieldListFilter):
             (
                 "내일",
                 {
-                    self.lookup_kwarg_since: str(today),
-                    self.lookup_kwarg_until: str(tomorrow),
+                    self.lookup_kwarg_since: str(tomorrow),
+                    self.lookup_kwarg_until: str(day_after_tomorrow),
                 },
             ),
         )
@@ -37,8 +38,8 @@ class CustomDateFieldListFilter(DateFieldListFilter):
             (
                 "모레",
                 {
-                    self.lookup_kwarg_since: str(tomorrow),
-                    self.lookup_kwarg_until: str(day_after_tomorrow),
+                    self.lookup_kwarg_since: str(day_after_tomorrow),
+                    self.lookup_kwarg_until: str(two_days_after_tomorrow),
                 },
             ),
         )
