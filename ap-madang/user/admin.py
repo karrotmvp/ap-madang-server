55ff12968982c35b1ff330dde3ae899411710568
ImagineHJ <hj0816hj@gmail.com>
2021-11-04 11:09:14 +0900

    feat: add column to admin

--- ap-madang/user/admin.py
+++ ap-madang/user/admin.py
@@ -4,5 +4,4 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "nickname", "karrot_user_id", "manner_point")
-    list_filter = ["nickname"]
+    list_display = ("id", "nickname", "manner_point", "created_at")
