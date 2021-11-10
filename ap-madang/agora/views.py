cb1f6477bca556329149168d38542d985d9f61cf
ImagineHJ <hj0816hj@gmail.com>
2021-11-10 19:42:18 +0900

    feat: add generate token logic

--- ap-madang/agora/views.py
+++ ap-madang/agora/views.py
@@ -1,6 +1,9 @@
 import random
 import string
 import datetime
+from AgoraDynamicKey.RtcTokenBuilder import *
+from config.settings import AGORA_APP_ID, AGORA_APP_CERTIFICATE
+
 
 # Create your views here.
 def create_channel_name():
@@ -8,4 +11,15 @@ def create_channel_name():
         str(datetime.datetime.now())
         + "_"
         + "".join(random.choices(string.ascii_letters + string.digits, k=20))
+    )
+
+
+def create_agora_token(meeting, user):
+    return RtcTokenBuilder.buildTokenWithUid(
+        AGORA_APP_ID,
+        AGORA_APP_CERTIFICATE,
+        meeting.channel_name,
+        user.id,
+        Role_Attendee,
+        0,
     )
