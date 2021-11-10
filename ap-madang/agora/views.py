6961d988a6cbea14f47d4bb76e284021eba6411c
ImagineHJ <hj0816hj@gmail.com>
2021-11-10 17:23:52 +0900

    feat: add create_channel_name method

--- ap-madang/agora/views.py
+++ ap-madang/agora/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import random
+import string
+import datetime
 
 # Create your views here.
+def create_channel_name():
+    return (
+        str(datetime.datetime.now())
+        + "_"
+        + "".join(random.choices(string.ascii_letters + string.digits, k=20))
+    )
