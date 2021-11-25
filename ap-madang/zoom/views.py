f6b192985fa9f679cd03bfe300d92d8aea58aeab
ImagineHJ <hj0816hj@gmail.com>
2021-11-25 18:21:28 +0900

    fix: encode error

--- ap-madang/zoom/views.py
+++ ap-madang/zoom/views.py
@@ -1,17 +1,18 @@
-import jwt, json, base64, requests
-from datetime import datetime
+import jwt, json, requests
+from datetime import datetime, timedelta
 from config.settings import ZOOM_API_KEY, ZOOM_API_SECRET
 
 
 def generate_jwt_for_zoom():
-    header = base64.b64encode({"alg": "HS256", "typ": "JWT"})
-    payload = base64.b64encode(
+    token = jwt.encode(
         {
             "iss": ZOOM_API_KEY,
-            "exp": datetime.now() + datetime.timedelta(minutes=90).timestamp(),
-        }
+            "exp": (datetime.now() + timedelta(minutes=90)).timestamp(),
+        },
+        ZOOM_API_SECRET,
+        algorithm="HS256",
+        headers={"alg": "HS256", "typ": "JWT"},
     )
-    token = jwt.encode(payload, ZOOM_API_SECRET, algorithm="HS256", headers=header)
     return token
 
 
@@ -22,7 +23,10 @@ def create_zoom_meeting(meeting):
         {
             "topic": meeting.meeting.title,
             "type": "2",
-            "start_time": str(meeting.date) + "T" + meeting.meeting.start_time + "Z",
+            "start_time": meeting.date.strftime("%Y-%m-%d")
+            + "T"
+            + meeting.meeting.start_time.strftime("%H:%M:%S")
+            + "Z",
             "duration": "60",
             "timezone": "Asia/Seoul",
             "settings": {
