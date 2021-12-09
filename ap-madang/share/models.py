08a27810ed06052509be0bf2de364979f5dbfa0a
ImagineHJ <hj0816hj@gmail.com>
2021-12-09 17:29:56 +0900

    feat: add short url model

--- ap-madang/share/models.py
+++ ap-madang/share/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ShareUrl(Base):
+    origin_url = models.CharField(max_length=200)
+    karrot_scheme_url = models.TextField(blank=True, null=True)
+    code = models.CharField(max_length=100)
+
+    class Meta:
+        indexes = [models.Index(fields=["origin_url"]), models.Index(fields=["code"])]
