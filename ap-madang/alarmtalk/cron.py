b9fdfa19075f6dc1b99d9e2ee20b14a109cf0516
ImagineHJ <hj0816hj@gmail.com>
2021-12-13 21:54:58 +0900

    feat: add hour left alarmtalk

--- ap-madang/alarmtalk/cron.py
+++ ap-madang/alarmtalk/cron.py
@@ -6,6 +6,13 @@ from django.db.models import F
 
 
 def send_meeting_alarm():
+    total_alarm_num = 0
+    print(
+        "----- meeting related alarm talk send start : "
+        + str(datetime.now())
+        + " -----"
+    )
+
     now = datetime.strftime(datetime.now(), "%H:%M:%000")
 
     # 현재 시간에 열리는 모임 리스트 가져오기
@@ -19,12 +26,15 @@ def send_meeting_alarm():
     ).filter(sent_at=None, meeting__in=meetings)
 
     # 오픈 알람톡 보내기
-    send_meeting_start_alarm_talk_to_owners(meetings)
-    send_meeting_start_alarm_talk(alarm_list)
+    total_alarm_num += send_meeting_start_alarm_talk_to_owners(meetings)
+    total_alarm_num += send_meeting_start_alarm_talk(alarm_list)
 
     # 현재 시간에 끝나는 모임 리스트 가져오기
     meetings_today = MeetingLog.objects.filter(
-        meeting__is_deleted=False, meeting__end_time=now, date=date.today()
+        meeting__is_deleted=False,
+        meeting__end_time=now,
+        date=date.today(),
+        meeting__start_time__lt=models.F("meeting__end_time"),
     )
 
     # 어제 시작됐는데, 오늘 끝난 모임
@@ -43,4 +53,32 @@ def send_meeting_alarm():
     ).filter(meeting_review_alarm_sent_at=None, meeting__in=meetings)
 
     # 종료/후기 알람톡 보내기
-    send_meeting_end_alarm_talk(enter_list)
+    total_alarm_num += send_meeting_end_alarm_talk(enter_list)
+
+    after_one_hour_date = (datetime.now() + timedelta(hours=1)).date()
+    after_one_hour_time = datetime.strftime(
+        datetime.now() + timedelta(hours=1), "%H:%M:%000"
+    )
+
+    # 한시간 뒤에 열리는 모임 리스트 가져오기
+    meetings = MeetingLog.objects.prefetch_related("meeting", "meeting__user").filter(
+        meeting__is_deleted=False,
+        meeting__start_time=after_one_hour_time,
+        date=after_one_hour_date,
+    )
+    # 해당 모임 예약 내역 가져오기
+    alarm_list = UserMeetingAlarm.objects.prefetch_related(
+        "user", "meeting", "meeting__meeting"
+    ).filter(sent_at=None, meeting__in=meetings)
+
+    # 한 시간 뒤 오픈 알람톡 보내기
+    total_alarm_num += send_meeting_start_in_hour_alarm_talk_to_owners(meetings)
+    total_alarm_num += send_meeting_start_in_hour_alarm_talk(alarm_list)
+
+    print(
+        "----- meeting related alarm talk send end : "
+        + str(datetime.now())
+        + "with alarm talks total ",
+        total_alarm_num,
+        "-----",
+    )
--- ap-madang/lambda/add_event_log.py
+++ ap-madang/lambda/add_event_log.py
@@ -8,22 +8,49 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 ##Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred
-client = pymongo.MongoClient(
-    "mongodb://{}:{}@meetup-events.cluster-c8zwh9jcttzy.ap-northeast-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false".format(
-        mongo_config.mongo_username, mongo_config.mongo_password
-    )
-)
+
+# client = pymongo.MongoClient("mongodb://{}:{}@meetup-event.buzao.mongodb.net/events?retryWrites=true&w=majority".format(
+#         mongo_config.mongo_username, mongo_config.mongo_password
+#     ))
+# db = client.test
+
+# client = pymongo.MongoClient(
+#     "mongodb://{}:{}@meetup-events.cluster-c8zwh9jcttzy.ap-northeast-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false".format(
+#         mongo_config.mongo_username, mongo_config.mongo_password
+#     )
+# )
+# client = MongoClient("meetup-events.cluster-c8zwh9jcttzy.ap-northeast-2.docdb.amazonaws.com", 27017, username=mongo_config.mongo_username, password=mongo_config.mongo_password, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
 # Set database
-db = client.dev
+# db = client.dev
 
 
 def lambda_handler(event, context):
-    body = event["body"]
+    logger.info("?!?!?!?")
+    print("!!")
+    try:
+        client = pymongo.MongoClient(
+            "mongodb+srv://{}:{}@meetup-event.buzao.mongodb.net/events?retryWrites=true&w=majority".format(
+                mongo_config.mongo_username, mongo_config.mongo_password
+            )
+        )
+        db = client.events
+        collection = db.dev
+    except:
+        logger.error("Failed to connect to MongoDB")
+        print("!!")
+    print(client.list_database_names())
 
-    events = db.events
+    # events = db.events
 
-    event.insert_one({"hello": "Amazon DocumentDB"})
+    collection.insert_one({"hello": "Amazon DocumentDB"})
 
     # TODO implement
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda!"),
+        "db": str(client.list_database_names()),
+    }
+
+
+lambda_handler(None, None)
