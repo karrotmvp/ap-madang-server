f5a77a1bbdc31b41eeaf43168f63f1b893e2667b
ImagineHJ <hj0816hj@gmail.com>
2021-12-16 22:34:03 +0900

    chore: add query for test

---
+++ ap-madang/lambda/image_resize.py
@@ -0,0 +1,84 @@
+import boto3
+import uuid
+from urllib.parse import unquote_plus
+from PIL import Image
+import pymysql
+import config
+import logging
+import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3_client = boto3.client("s3")
+
+
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
+        image.thumbnail(tuple(x / 2 for x in image.size))
+        image.save(resized_path, format="webp")
+
+
+def update_thumbnail(meeting_id, thumbnail_image_url):
+    # db setting
+    try:
+        conn = pymysql.connect(
+            host=config.dev_host,
+            user=config.dev_username,
+            password=config.dev_password,
+            db=config.dev_name,
+        )
+    except pymysql.MySQLError as e:
+        logger.error("Failed to connect to MySQL")
+        return {"success": False, "message": "Database Error"}
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    update_sql = "UPDATE meeting_meeting SET updated_at='{}', thumbnail_image_url='{}' WHERE id={};".format(
+        datetime.datetime.now(), thumbnail_image_url, meeting_id
+    )
+    cursor.execute(update_sql)
+
+    conn.commit()
+    conn.close()
+
+
+def lambda_handler(event, context):
+    for record in event["Records"]:
+        print(record)
+        # bucket = record["s3"]["bucket"]["name"]
+        # key = unquote_plus(record["s3"]["object"]["key"])
+
+        bucket = record["messageAttributes"]["bucket"]["stringValue"]
+        key = record["messageAttributes"]["key"]["stringValue"]
+        meeting_id = record["messageAttributes"]["meeting_id"]["stringValue"]
+
+        print(bucket, key, meeting_id)
+
+        # 기존 파일 경로 및 이름"media/meeting_image/" + file_name
+        path = key.split("/")[0] + "/" + key.split("/")[1]
+        file_name_ext = key.split("/")[-1]
+        file_name = file_name_ext.split(".")[0] + "." + file_name_ext.split(".")[1]
+
+        tmpkey = key.replace("/", "")
+        download_path = "/tmp/{}{}".format(uuid.uuid4(), tmpkey)
+        upload_path = "/tmp/resized-{}".format(tmpkey)
+
+        s3_client.download_file(bucket, key, download_path)
+        resize_image(download_path, upload_path)
+        thumbnail_file_name = "{}.{}".format(file_name, "webp")
+        s3_client.upload_file(
+            upload_path,
+            config.thumbnail_bucket_name,
+            thumbnail_file_name,
+            ExtraArgs={"ContentType": "image/webp"},
+        )
+        original_image_url = "meeting_image/" + file_name_ext
+        thumbnail_image_url = "https://{}.s3.{}.amazonaws.com/{}".format(
+            config.thumbnail_bucket_name, config.bucket_region, thumbnail_file_name
+        )
+        update_thumbnail(meeting_id, thumbnail_image_url)
+        logger.info(
+            "----- sucessfully uploaded file {} -----".format(thumbnail_image_url)
+        )
+
+    return {"statusCode": 200, "body": "thumbnail image resize sucess"}
