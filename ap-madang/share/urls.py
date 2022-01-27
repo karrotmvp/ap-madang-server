422121a8b3c36d3925b590eaaa74c4bdcc93bf47
ImagineHJ <hj0816hj@gmail.com>
2022-01-27 14:46:44 +0900

    feat: add get karrot scheme url

--- ap-madang/share/urls.py
+++ ap-madang/share/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("short-url/meeting", get_or_create_short_url),
     path("redirect", redirect_to_karrot_scheme),
+    path("karrot-scheme-url", get_karrot_scheme_url),
 ]
