c38d97a1828f7af9fce072dde5c5da0ecae721c2
ImagineHJ <hj0816hj@gmail.com>
2022-02-04 16:11:27 +0900

    feat: add meeting link close api

--- ap-madang/share/views.py
+++ ap-madang/share/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from config.settings import CLIENT_BASE_URL
 from .models import *
 from .utils import *
+from meeting.models import MeetingLog
 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
@@ -33,6 +34,18 @@ def redirect_to_karrot_scheme(request):
 def get_karrot_scheme_url(request):
     code = request.GET.get("share_code", None)
     short_url = get_object_or_404(ShareUrl, code=code)
+
+    try:
+        # 모임에 대한 code 인 경우
+        meeting_log = MeetingLog.objects.get(id=code[5:])
+        # 링크 모임이면서, 모임이 종료된 경우 -> CLOSED 스킴을 반환한다
+        if (meeting_log.closed_at is not None) and (meeting_log.meeting.is_link):
+            short_url = get_object_or_404(ShareUrl, code="CLOSED")
+
+    except MeetingLog.DoesNotExist:
+        # 모임에 대한 code 가 아닌 경우
+        pass
+
     short_url.access_cnt += 1
     short_url.save()
     return JsonResponse(
