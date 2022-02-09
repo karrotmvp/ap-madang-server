9188be9b43cc800d79a1fb8077b7385efb7877ee
ImagineHJ <hj0816hj@gmail.com>
2022-02-09 15:12:56 +0900

    feat: modify alarmtalk logic

--- ap-madang/alarmtalk/cron.py
+++ ap-madang/alarmtalk/cron.py
@@ -1,3 +1,4 @@
+from time import time
 from meeting.models import UserMeetingEnter
 from .models import *
 from .views import *
@@ -12,8 +13,11 @@ def send_meeting_alarm():
         + str(datetime.now())
         + " -----"
     )
-
-    now = datetime.strftime(datetime.now(), "%H:%M:%000")
+    now_datetime = datetime.now()
+    now = datetime.strftime(now_datetime, "%H:%M:%000")
+    thirty_mins_ago = datetime.strftime(
+        now_datetime - timedelta(minutes=30), "%H:%M:%000"
+    )
 
     # 현재 시간에 열리는 모임 리스트 가져오기
     meetings = MeetingLog.objects.prefetch_related("meeting", "meeting__user").filter(
@@ -29,18 +33,22 @@ def send_meeting_alarm():
     total_alarm_num += send_meeting_start_alarm_talk_to_owners(meetings)
     total_alarm_num += send_meeting_start_alarm_talk(alarm_list)
 
-    # 현재 시간에 끝나는 모임 리스트 가져오기
+    # 시간 비교를 위해 변환
+    if now == "00:00:00":
+        now = "23:59:59"
+
+    # 근 30분 이내에 끝난 모임 리스트 가져오기
     meetings_today = MeetingLog.objects.filter(
         meeting__is_deleted=False,
-        meeting__end_time=now,
+        meeting__end_time__range=[thirty_mins_ago, now],
         date=date.today(),
         meeting__start_time__lt=models.F("meeting__end_time"),
     )
 
-    # 어제 시작됐는데, 오늘 끝난 모임
+    # 어제 시작됐는데, 근 30분 이내에 끝난 모임
     meetings_yesterday = MeetingLog.objects.filter(
         meeting__is_deleted=False,
-        meeting__end_time=now,
+        meeting__end_time__range=[thirty_mins_ago, now],
         date=date.today() - timedelta(days=1),
         meeting__start_time__gt=models.F("meeting__end_time"),
     )
